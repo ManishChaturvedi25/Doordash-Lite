2ff190cb5b275e22fe9024f3804db3562c00ee5f
Manish Chaturvedi <manish.chaturvedi7@yahoo.com>
2017-05-12 06:33:57 -0700

    Added Favorite View.

---
+++ DD/ResultsDetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ResultsDetailViewController.swift
+//  DD
+//
+//  Created by ctsuser1 on 5/12/17.
+//  Copyright © 2017 ManishChaturvedi. All rights reserved.
+//
+
+import UIKit
+
+class ResultsDetailViewController: UITableViewController {
+
+
+    @IBOutlet weak var storeImageView: UIImageView!
+    
+    @IBOutlet weak var arrivalDetailsLabel: UILabel!
+    
+    @IBOutlet weak var favouriteButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func addToFavourite(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
